1a866970ea86d0cb9acde52e053f4b1ab3c1e5a7
naisarg14 <naisarg.patel14@hotmail.com>
2024-07-04 15:05:46 +0530

    part1 complete

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,34 +1,80 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const Heading = ({text}) => {
+  return (
+      <h1>{text}</h1>
+  )
+}
+
+
+const Button = ({text, handleClick}) => {
+  return (
+      <div>
+        <button onClick={handleClick} >{text}</button>
+      </div>
+  )
+}
+
+const DisplayVote = ({votes}) => {
+  return (
+    <div>
+      Has {votes} Votes
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [allClicks, setAll] = useState(Array(anecdotes.length).fill(0))
+
+  const randValue = () => {
+    let x = Math.floor(Math.random() * (anecdotes.length))
+    setSelected(x)
+  }
+
+  const voteAnecdote = () => {
+    const copy = [...allClicks]
+    copy[selected] += 1
+    setAll(copy)
+  }
+
+  const bestQuote = () => {
+    let max = allClicks[0];
+    let maxIndex = 0;
+
+    for (let i = 0; i < allClicks.length; i++) {
+        if (allClicks[i] > max) {
+            maxIndex = i;
+            max = allClicks[i];
+        }
+    }
+    return maxIndex;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Heading text="Anecdote of the Day" />
+      {anecdotes[selected]}
+      <DisplayVote votes={allClicks[selected]} />
+      <Button text="Vote Anecdote" handleClick={() => {voteAnecdote()}} />
+      <Button text="Next Anecdote" handleClick={() => {randValue()}} />
+    
+      <Heading text="Anecdote with most Votes" />
+      {anecdotes[bestQuote()]}
+      <DisplayVote votes={allClicks[bestQuote()]} />
+    </div>
   )
 }
 
